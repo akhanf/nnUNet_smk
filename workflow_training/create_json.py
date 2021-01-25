8bc2269d4981ee586499282ce6b3de794d905a0f
jordandekraker <jordandekraker@gmail.com>
2021-01-25 12:38:47 -0500

    minor typo fix

--- workflow_training/create_json.py
+++ workflow_training/create_json.py
@@ -8,9 +8,9 @@ dataset['training'] = [{'image': img, 'label': lbl} for img,lbl in zip(
     snakemake.input.training_imgs,
     snakemake.input.training_lbls)]
 
-dataset['modality'] = [{name: num} for name,num in zip(
-    snakemake.params.modalities,
-    [str(i).zfill(4) for i in range(0,len(snakemake.params.modalities))])]
+dataset['modality'] = [{num: name} for num,name in zip(
+    [str(i).zfill(4) for i in range(0,len(snakemake.params.modalities))],
+    snakemake.params.modalities)]
 
 dataset['numTraining'] = len(dataset['training'])
 
